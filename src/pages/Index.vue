bb5f6693b657c445e1d539f80ecc836faa16ccee
ebool <lastdroq@gmail.com>
2019-08-03 03:39:16 +0900

    add stats <-> home transition

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,13 +1,32 @@
 <template>
-  <router-view></router-view>
+  <transition :name="transitionName" mode="out-in">
+    <router-view></router-view>
+  </transition>
 </template>
 
 <script>
 export default {
-  components: {
+  data () {
+    return {
+      transitionName: ''
+    }
+  },
+  watch: {
+    '$route' (to, from) {
+      let res = to.path.indexOf('stats') > 0 && from.path.indexOf('home') > 0;
+      let res1 = from.path.indexOf('stats') > 0 && to.path.indexOf('home') > 0;
+      this.transitionName = res || res1 ? 'rotate' : '';
+    }
   }
 }
 </script>
 
-<style>
+<style scoped lang="scss">
+  .rotate-enter-active, .rotate-leave-active {
+    transition: all .2s;
+  }
+  .rotate-enter, .rotate-leave-to {
+    opacity: 0;
+    transform: rotateY(90deg);
+  }
 </style>
